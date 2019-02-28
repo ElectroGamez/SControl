d13215a383067693ebac50e26451fd405b166277
ElectroGamez <git@electrogamez.nl>
2019-02-28 18:52:24 +0100

    Now using johnny-five for arduino

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,38 +2,32 @@ var express        =         require("express");
 var bodyParser     =         require("body-parser");
 var app            =         express();
 
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
-
-const port = new SerialPort("COM5", { baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/',function(req,res){
-  res.sendfile("index.html");
+var five = require("johnny-five");
+var board = new five.Board({
+  port: "COM5"
 });
 
-app.post('/', function(req, res){
-  console.log('Got data from web:', req.body.data);
+board.on("ready", function() {
+  var servo = new five.Servo(10);
 
-  port.write(req.body.data, (err) => {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  app.get('/',function(req,res){
+    res.sendfile("index.html");
   });
-  res.end();
-});
 
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
-})
+  app.post('/servo', function(req, res){
+    let data = req.body.data;
+    
+    if (isNaN(data)) return console.log("Not a number");
+    servo.to(data);
+    res.end();
+  });
 
-// Read the port data
-port.on("open", () => {
-  console.log('serial port open');
-});
-parser.on('data', data =>{
-  console.log('got word from arduino:', data);
+  app.listen(3000,function(){
+    console.log("Started on PORT 3000");
+  });
+
 });
